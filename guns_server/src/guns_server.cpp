5df9b7070e7f1025541d9c60ecd989deb40fe8a1
Octav Sandulescu <github@narc.ro>
2011-08-14 16:15:44 +0300

    Minimum necessary to build guns_server.

---
+++ guns_server/src/guns_server.cpp
@@ -0,0 +1,18 @@
+﻿/*
+
+ Main entry point for the Guns server.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	cout << "Guns! dedicated server, v. 0.01. © 2011 narc0tiq." << endl;
+
+	cout << "Doesn't do anything yet, sorry!" << endl;
+
+	return 0;
+}
