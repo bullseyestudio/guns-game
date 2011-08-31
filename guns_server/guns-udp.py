821c131d33c30cc71f0805d99893a59846211ce1
Steven Harrison <eztarget.uk@gmail.com>
2011-08-31 22:49:07 +0100

    Updated the network comms to send basic information about players on the server, also changed the amount of data that is sent from the server to the clients every tick.

--- guns_server/guns-udp.py
+++ guns_server/guns-udp.py
@@ -2,6 +2,7 @@
 
 import os, sys
 import socket, select
+from collections import deque
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0', 45005))
@@ -10,13 +11,88 @@ width = 1024
 height = 576
 
 class Player:
+	plrCount = 0;
 	def __init__(self):
 		self.velocity = [0,0]
 		self.position = [0,0]
 		self.addr = None
 		self.name = ''
+		Player.plrCount += 1
+		self.id = Player.plrCount
+		self.moved = 0
+		self.active = 0
 
 players = {}
+
+def findPlayerByAddr( addr ):
+	ret = None
+	for id, p in players.iteritems():
+		if p.addr == addr:
+			ret = p
+	return ret
+
+def findPlayerByName( name ):
+	ret = None
+	for id, p in players.iteritems():
+		if p.name == name:
+			ret = p
+	return ret
+
+def EDIDecoder( EDI, addr ):
+	EDIargs = EDI.split( ' ' )
+	
+	print EDIargs
+	
+	if   EDIargs[0] == 'LAG':
+		print 'Ping test, please.'
+	elif EDIargs[0] == 'MSG':
+		print 'Message to everyone.'
+	elif EDIargs[0] == 'MST':
+		print 'Message to team'
+	elif EDIargs[0] == 'USA':
+		print 'User \'{0}\' connected to the server.'.format( EDIargs[ 1 ] )
+		p = findPlayerByName( EDIargs[1] )
+		if not p == None:
+			if p.active == 0:
+				p.velocity[0] = 0
+				p.velocity[1] = 0
+				p.addr = addr
+				p.active = 1
+			else:
+				print 'HACK ATTEMPT'
+		else:
+			p = Player()
+			p.velocity[0] = 0
+			p.velocity[1] = 0
+			p.addr = addr
+			p.name = EDIargs[ 1 ]
+			p.active = 1
+			players[ p.id ] = p
+		data = 'USI {0}'.format( p.id )
+		sock.sendto( data, addr )
+		updatePlayers( p.id )
+	elif EDIargs[0] == 'USC':
+		print 'Hey I should be a different colour'
+	elif EDIargs[0] == 'USD':
+		print 'Rage quit'
+		uid = int( EDIargs[1] )
+		if uid in players:
+			del players[uid]
+	elif EDIargs[0] == 'USP':
+		print 'I moved.'
+		p = players[ int( EDIargs[1] ) ]
+		p.velocity[0] = int( EDIargs[2] )
+		p.velocity[1] = int( EDIargs[3] )
+		
+		if ( p.velocity[0] == 0 ) and ( p.velocity[1] == 0 ):
+			p.moved = 0
+			print '{0} stopped'.format( p.id )
+		else:
+			p.moved = 1
+			print '{0} moving'.format( p.id )
+	else:
+		print 'Errrr...'
+	return
 
 def check_for_playerinput():
 	socks = select.select([sock], [], [], 0)
@@ -28,29 +104,7 @@ def check_for_playerinput():
 	data = data.strip()
 	print 'got data: ', data, 'from', addr
 
-	data_parts = data.split(' ')
-	if len(data_parts) == 2:
-		user, command = data_parts
-		if user in players: del players[user]
-		return
-	elif len(data_parts) == 3:
-		user, xvel, yvel = data_parts
-
-	if user in players:
-		p = players[user]
-
-		print 'updating velocity for', p
-
-		p.velocity[0] = int(xvel)
-		p.velocity[1] = int(yvel)
-		p.addr = addr
-	else:
-		p = Player()
-		p.velocity[0] = int(xvel)
-		p.velocity[1] = int(yvel)
-		p.name = user
-		p.addr = addr
-		players[user] = p
+	EDIDecoder( data, addr )
 
 def move_players():
 	for user, p in players.iteritems():
@@ -63,13 +117,40 @@ def move_players():
 			p.velocity[1] = -p.velocity[1]
 
 def tell_players():
-	data = ''
+	data = {}
 
-	for user, p in players.iteritems():
-		data += '{0} {1[0]} {1[1]}\r\n'.format(p.name, p.position)
+	for uid, p in players.iteritems():
+		if p.moved == 1:
+			data[ uid ] = 'USM {0} {1[0]} {1[1]}'.format(uid, p.position)
+			#p.moved = 0
 
-	for user, p in players.iteritems():
-		sock.sendto(data, p.addr)
+	for uid, p in players.iteritems():
+		for id, d in data.iteritems():
+			sock.sendto(d, p.addr)
+			print d
+
+def updatePlayers( pid ):
+	data = deque('')
+
+	for uid, p in players.iteritems():
+		if not p.id == pid:
+			data.append( 'USJ {0} {1}'.format(p.name, uid) )
+			data.append( 'USM {0} {1[0]} {1[1]}'.format(uid, p.position) )
+	
+	p = players[ pid ]
+	for d in data:
+		sock.sendto(d, p.addr)
+		print d
+	
+	data.clear()
+	
+	data.append( 'USJ {0} {1}'.format(p.name, pid) )
+	data.append( 'USM {0} {1[0]} {1[1]}'.format(pid, p.position) )
+
+	for uid, p in players.iteritems():
+		for d in data:
+			sock.sendto(d, p.addr)
+			print d
 
 def timer_tick():
 	check_for_playerinput()
