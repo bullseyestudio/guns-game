18609dfa20bf2d62096c0ea44be6415878b2a1b8
Octav Sandulescu <github@narc.ro>
2011-09-11 16:06:59 +0300

    De-stubbed the edicomm.decode function. Looks like a good implementation!

--- common/modules/edicomm.py
+++ common/modules/edicomm.py
@@ -6,7 +6,50 @@ TODO: More docstringy stuff goes here.
 
 def decode(what):
 	"""Decode a EDIComm string into its component parts (splitting lists as needed)"""
-	pass
+
+	parts = what.split(' ')
+	new_parts = []
+
+	# First expansion: each separate parameter, keeping lists together.
+	this_part = ''
+	for part in parts:
+		this_part += part
+
+		merge = part.endswith('\\')
+
+		if merge:
+			this_part = this_part[:-1] + ' '
+		else:
+			new_parts.append(this_part)
+			this_part = ''
+
+	parts = new_parts
+	new_parts = []
+
+	# Second expansion: lists.
+	for part in parts:
+		if part.find(',') == -1:
+			new_parts.append(part)
+			continue
+
+		part_parts = part.split(',')
+
+		parsed_parts = []
+		parsed = ''
+		for p in part_parts:
+			parsed += p
+
+			merge = p.endswith('\\')
+
+			if merge:
+				parsed = parsed[:-1] + ','
+			else:
+				parsed_parts.append(parsed)
+				parsed = ''
+
+		new_parts.append(parsed_parts)
+
+	return new_parts
 
 def encode(stuff):
 	"""Encode components into a proper EDIComm string.
