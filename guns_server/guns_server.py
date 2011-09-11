154b7f6bc724cde8ea5bb26de5c834385e382ef3
Octav Sandulescu <github@narc.ro>
2011-09-11 12:33:48 +0300

    Added readline support. Yeah, it's that easy.

--- guns_server/guns_server.py
+++ guns_server/guns_server.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import atexit
+
+try:
+	import pyreadline as readline
+except ImportError:
+	try:
+		import readline
+	except ImportError:
+		sys.stderr.write('Cannot import libreadline. You\'ll have a pretty crippled command line. Sorry.\n')
+
 
 sys.path.append('./modules')
 sys.path.append('../common/modules')
