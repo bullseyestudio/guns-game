a62f68e832a011bbfea1d1cbc0db55e5df8676d8
Octav Sandulescu <github@narc.ro>
2011-10-03 17:40:35 +0300

    Changed velocity code to match personal preferences

--- guns_client/modules/input_handler.py
+++ guns_client/modules/input_handler.py
@@ -17,7 +17,7 @@ except ImportError, err:
 
 def init_joy( joynum ):
 	global_.joystick_count = pygame.joystick.get_count()
-	
+
 	if global_.joystick_count == 0:
 		print "No joysticks detected, joystick support not enabled."
 	else:
@@ -26,28 +26,32 @@ def init_joy( joynum ):
 
 def keyboard( event ):
 	move = False
-	
+
 	print '{0}:{1}'.format( event.type, event.key )
-	
+
 	if event.type == KEYDOWN:
 		if event.key == K_s:
-			global_.velocity[1] = 50
+			global_.velocity[1] += 50
 		elif event.key == K_w:
-			global_.velocity[1] = -50
-		elif event.key == K_a:
-			global_.velocity[0] = -50
+			global_.velocity[1] += -50
 		elif event.key == K_d:
-			global_.velocity[0] = 50
+			global_.velocity[0] += 50
+		elif event.key == K_a:
+			global_.velocity[0] += -50
 		elif event.key == K_z:
 			global_.velocity = [0, 0]
 		elif event.key == K_x:
 			print global_.players.items()
 	elif event.type == KEYUP:
-		if event.key in [K_s, K_w]:
-			global_.velocity[1] = 0
-		if event.key in [K_a, K_d]:
-			global_.velocity[0] = 0
-			
+		if event.key == K_s:
+			global_.velocity[1] += -50
+		elif event.key == K_w:
+			global_.velocity[1] += 50
+		elif event.key == K_d:
+			global_.velocity[0] += -50
+		elif event.key == K_a:
+			global_.velocity[0] += 50
+
 	network_comms.send( edicomm.encode( 'USV', global_.velocity ) )
 
 def joystick( event ):
@@ -55,4 +59,3 @@ def joystick( event ):
 	global_.velocity[0] = int( global_.my_joystick.get_axis( 0 ) * 50 )
 	global_.velocity[1] = int( global_.my_joystick.get_axis( 1 ) * 50 )
 	network_comms.send( edicomm.encode( 'USV', global_.velocity ) )
-
