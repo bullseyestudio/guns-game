d3f2026344ebee9df092e34bc4361df845b3b876
Octav Sandulescu <github@narc.ro>
2011-10-13 05:36:56 +0300

    Moved server-wide constants to their own module

--- guns_server/modules/battle.py
+++ guns_server/modules/battle.py
@@ -5,10 +5,11 @@
 
 import socket, select
 import edicomm
+import constants
 from math import atan2, degrees
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', 45005))
+sock.bind(constants.listen_addr)
 
 width = 1024
 height = 576
---
+++ guns_server/modules/constants.py
@@ -0,0 +1,3 @@
+""" Server-wide constants. """
+
+listen_addr = ('0.0.0.0', 45005)
