18a5afaed3556648b7fa5eed3212d59e6b1b12a8
Octav Sandulescu <github@narc.ro>
2011-10-18 12:30:26 +0300

    Bullets now delete themselves when they're 20 ticks old
    - also animate at a much faster rate... which feels okay.

--- guns_client/modules/bullet.py
+++ guns_client/modules/bullet.py
@@ -12,24 +12,29 @@ class Bullet:
 		self.sizechange = 1
 
 	def redraw( self, screen ):
+		global all
+
 		selfpos = ( int( self.position[0] * constants.zoom) , int( self.position[1] * constants.zoom ) )
 		plrpos = ( int( constants.cplr.position[0] * constants.zoom) , int( constants.cplr.position[1] * constants.zoom ) )
+
+		self.timer += 1
+
+		if self.timer > 10:
+			self.sizechange = -1
+
+		if self.timer > 20:
+			all.remove(self)
+
 		if self.size > 0:
 			srf = pygame.Surface( ( self.size * 2, self.size * 2 ) )
-			#opos = [ self.position[0] - constants.cplr.position[0] + ( constants.screen.get_width() /2 ), self.position[1] - constants.cplr.position[1] + ( constants.screen.get_height() /2 ) ]
+
 			opos = [ selfpos[0] - plrpos[0] +  ( constants.screen.get_width() /2 ), selfpos[1] - plrpos[1] +  ( constants.screen.get_height() /2 ) ]
 			pos = [a - b for a, b in zip(opos, (self.size, self.size))]
 
 			pygame.draw.rect( srf, ( 0, 0, 0 ), srf.get_rect() )
 			constants.screen.blit( srf, pos )
 
-			if self.timer > 10:
-				self.size = self.size + self.sizechange
-				self.timer = 0
+			self.size = self.size + self.sizechange
 
-				if self.size > 10:
-					self.sizechange = -1
-
-			self.timer = self.timer + 1
 
 all = []
