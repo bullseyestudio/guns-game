b5def83c20a4d6b1b52414de4b770e62790343b1
Octav Sandulescu <github@narc.ro>
2011-11-09 03:09:18 +0200

    Clean slate to start client refactoring on.
    We'll be stealing bits back out of the old client as we go.

--- client.py
+++ client.py
@@ -2,31 +2,12 @@
 
 import sys
 
-from modules import edicomm
-from modules.client import config
-from modules.client import constants, gui, network_comms, input_handler, battle
-
-if len(sys.argv) < 3:
-	sys.stderr.write('Usage: client.py username server\r\n')
-	sys.exit(2)
-
-constants.username = sys.argv[1]
-constants.host = sys.argv[2]
-
 try:
 	import pygame
 	from pygame.locals import *
-except ImportError, err:
+except ImportError:
 	sys.stderr.write('This application absolutely requires pygame. Sorry.\r\n')
 	sys.exit(1)
 
-gui.init_display()
-network_comms.open()
-input_handler.init_joy( 0 )
-
-battle.init()
-
-network_comms.battle.send( edicomm.encode( 'UST', 'token_{0}'.format( constants.username ) ) )
-
-while not gui.done:
-	gui.event_loop()
+from modules import edicomm
+from modules.client import config
