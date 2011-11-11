d47454ed879dae14cd9d8ce52eddbfc433b9ad10
Octav Sandulescu <github@narc.ro>
2011-11-11 18:21:59 +0200

    Finally linked the Quit button correctly

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import sys, time
 
 try:
 	import pygame
+	import pygame._view # Py2exe needs this, else it won't include _view
 	from pygame.locals import *
 except ImportError:
 	sys.stderr.write('This application absolutely requires pygame. Sorry.\r\n')
@@ -17,16 +18,35 @@ from modules.client import config, gui
 config.read_config()
 gui.init_display()
 gui.show_logo()
+ms_atlogo = pygame.time.get_ticks()
 
 # Q'n'D demo code (delete me soonest)
 from modules.client.gui import mainmenu
 from modules.pgu import gui as pgui
 
 a = pgui.App()
-a.connect(pgui.QUIT, a.quit, None)
-mainmenu.b.connect(pgui.CLICK,a.quit,None)
+
+a.init(mainmenu.t, gui.common.screen)
+
+ms_afterinit = pygame.time.get_ticks()
+ms_to_wait = 2000 - (ms_afterinit - ms_atlogo)
+pygame.time.wait(ms_to_wait)
 
 gui.hide_logo()
+a.loop()
 
-a.run(mainmenu.t)
+c = pygame.time.Clock()
+
+quitting = False
+while not quitting:
+	elapsed = c.tick(60)
+
+	for ev in pygame.event.get():
+		if ev.type == pygame.QUIT:
+			quitting = True
+
+		a.event(ev)
+
+	rects = a.update(gui.common.screen)
+	pygame.display.update(rects)
 # End Q'n'D demo code
--- modules/client/gui/mainmenu.py
+++ modules/client/gui/mainmenu.py
@@ -1,24 +1,31 @@
-
+import pygame
 from modules.client.gui import common
-from modules.pgu import gui
+from modules.pgu import gui as pgui
+
+def quit_handler():
+	pygame.event.post(pygame.event.Event(pygame.QUIT))
 
 wid = common.screen.get_width()
 hgt = common.screen.get_height()
 
-t = gui.Table(width=wid, height=hgt)
+t = pgui.Table(width=wid, height=hgt)
 
 t.tr()
-t.td(gui.Image('data/GunsLogo.png', valign=-1), height=int(hgt/2))
+t.td(pgui.Image('data/GunsLogo.png', valign=-1), height=int(hgt/2))
 
 t.tr()
-t.td(gui.Button('Multiplayer', width=150, height=30))
+t.td(pgui.Button('Build a tank', width=150, height=30))
 
 t.tr()
-t.td(gui.Button('Options', width=150, height=30))
+t.td(pgui.Button('Multiplayer', width=150, height=30))
 
 t.tr()
-b = gui.Button('Quit', width=150, height=30)
+t.td(pgui.Button('Options', width=150, height=30))
+
+t.tr()
+b = pgui.Button('Quit', width=150, height=30)
+b.connect(pgui.CLICK, quit_handler)
 t.td(b)
 
 t.tr()
-t.td(gui.Spacer(width=150, height=int(hgt/2) - (3 * 40)))
+t.td(pgui.Spacer(width=150, height=int(hgt/2) - (4 * 40)))
